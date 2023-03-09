9967670f5a1dc6ecf97a7ca3e50872166d991eb5
Michael Mayaguari <mayaguarimichael@gmail.com>
2023-03-09 11:34:29 -0500

    login/logout routes added in backend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,36 @@
 class SessionsController < ApplicationController
-  def new
+  before_action :logged_in_redirect, only: [:new, :create]
 
+  def new; end
+
+  def create
+    user = User.find_by(username: login_params[:username])
+    if user && user.authenticate(login_params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong with your login information"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
+  end
+
+  private
+
+  def login_params
+    params.require(:session).permit(:username, :password)
+  end
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 end
