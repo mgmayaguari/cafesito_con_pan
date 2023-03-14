dd410bbe8d4909a91ba9202279c651cfd312ceb3
Michael Mayaguari <mayaguarimichael@gmail.com>
2023-03-14 17:49:17 -0400

    core chat features done

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,14 +5,26 @@ import * as jquery from "jquery"
 import "semantic-ui"
 import "channels"
 
+// Adds auto scroll to bottom in messages
+let auto_acroll = function () {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  };
+};
+
+// Uses an event to submit and clear input textfield
+let submit_button = function () {
+  $('#message_body').on('keydown', function (e) {
+    if (e.keyCode == 13) {
+      // $('button').trigger('click'); // Rails 7 already has this feature integrated
+      e.target.value = ""
+    };
+  });
+};
+
 $(document).on('turbo:load', function () {
   $('.ui.dropdown')
     .dropdown();
-})
-
-// add auto scroll to bottom in messages
-$(document).on('turbo:load', function() {
-  if ($('#messages').length > 0) {
-  $('#messages').scrollTop($('#messages')[0].scrollHeight);
-  }
+  auto_acroll();
+  submit_button();
 })
